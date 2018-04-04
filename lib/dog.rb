a67df3ff7c5a2bc042f4cd29149d4eb13215feb9
Dawn Catanach <dawn.catanach@gmail.com>
2018-04-04 23:57:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,14 @@
 # Your code goes here!
-class Dog 
-  #code 
-end 
+class Dog
+  def name=(name)
+    @name = name 
+  end 
+
+  def name 
+    @name 
+  end 
+
+  def bark 
+    puts "Woof!" 
+  end 
+end
